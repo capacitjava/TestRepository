62e833d69aaf769d5bffc787c00182cdc4df49a5
Admin <Admin@DESKTOP-F4J5IG2>
2019-10-18 12:54:44 -0500

    //SE CREO EL PACKAGE Y LA CLASE EMPLEADO, GET Y SET.

---
+++ TestProject/src/org/modelo/empleado/Empleado.java
@@ -0,0 +1,25 @@
+package org.modelo.empleado;
+
+public class Empleado {
+
+	private int id;
+	private String nombre;
+	
+	//GETTERS AND SETTERS
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+}
